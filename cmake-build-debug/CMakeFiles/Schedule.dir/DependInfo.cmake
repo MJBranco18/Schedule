1f20f97b31921621d4e0131ddcb93628ea8e149d
Xouteiro <up202108895@up.pt>
2022-10-21 22:29:31 +0100

    c

---
+++ cmake-build-debug/CMakeFiles/Schedule.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/35191/Faculdade/AED/Trabalho/Schedule/src/ClassUc.cpp" "CMakeFiles/Schedule.dir/src/ClassUc.cpp.obj" "gcc" "CMakeFiles/Schedule.dir/src/ClassUc.cpp.obj.d"
+  "C:/Users/35191/Faculdade/AED/Trabalho/Schedule/src/ScheduleManager.cpp" "CMakeFiles/Schedule.dir/src/ScheduleManager.cpp.obj" "gcc" "CMakeFiles/Schedule.dir/src/ScheduleManager.cpp.obj.d"
+  "C:/Users/35191/Faculdade/AED/Trabalho/Schedule/src/ScheduleUc.cpp" "CMakeFiles/Schedule.dir/src/ScheduleUc.cpp.obj" "gcc" "CMakeFiles/Schedule.dir/src/ScheduleUc.cpp.obj.d"
+  "C:/Users/35191/Faculdade/AED/Trabalho/Schedule/src/Slot.cpp" "CMakeFiles/Schedule.dir/src/Slot.cpp.obj" "gcc" "CMakeFiles/Schedule.dir/src/Slot.cpp.obj.d"
+  "C:/Users/35191/Faculdade/AED/Trabalho/Schedule/src/Student.cpp" "CMakeFiles/Schedule.dir/src/Student.cpp.obj" "gcc" "CMakeFiles/Schedule.dir/src/Student.cpp.obj.d"
+  "C:/Users/35191/Faculdade/AED/Trabalho/Schedule/src/main.cpp" "CMakeFiles/Schedule.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Schedule.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
