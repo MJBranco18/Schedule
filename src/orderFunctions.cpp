08b434c2ab31b41c9266d8a2a3f50b7157fc9cf1
coutinho21 <guilhermecscoutinho@gmail.com>
2022-11-04 17:06:36 +0000

    Finished the total and parcial listing and added some comments

--- src/orderFunctions.cpp
+++ src/orderFunctions.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <algorithm>
 
+/*
+ * Função de comparação para sort dos pairs da ocupação
+ * neste caso é ordem crescente
+ */
 bool occupationsCresc(std::pair<int,int> p1, std::pair<int,int> p2){
     if(p1.second < p2.second) return true;
     if(p1.second > p2.second) return false;
@@ -11,6 +15,10 @@ bool occupationsCresc(std::pair<int,int> p1, std::pair<int,int> p2){
     return false;
 }
 
+/*
+ * Função de comparação para sort dos pairs da ocupação
+ * neste caso é ordem decrescente
+ */
 bool occupationsDecresc(std::pair<int,int> p1, std::pair<int,int> p2){
     if(p1.second > p2.second) return true;
     if(p1.second < p2.second) return false;
@@ -20,6 +28,10 @@ bool occupationsDecresc(std::pair<int,int> p1, std::pair<int,int> p2){
     return false;
 }
 
+/*
+ * Função de comparação para sort da listagem de estudantes
+ * coloca o vetor de estudantes por ordem alfabética
+ */
 bool alphOrder(const Student& s1, const Student& s2){
     return s1.getName() < s2.getName();
 }
